cb080420d3155f28d3cb975c690fc8cf493e4e64
Jason Son <jasonson2004@gmail.com>
2025-03-19 04:44:49 -0700

    add(doc) phaser items used

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 // main.js
+/*
+ Phaser features used:
+ - Scene management
+ - Game object creation
+ - Animation handling
+ - Input handling
+ - Physics handling
+ - UI management
+ - Sound handling
+ - BitmapText
+ - Text
+ - Image
+ - Sprite
+ - Rectangle
+ - Circle
+*/
+
 const config = {
   type: Phaser.AUTO,
   width: 640,  // 10 squares * 64 pixels
